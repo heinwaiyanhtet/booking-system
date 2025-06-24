cc7f18e27465c69421959051c4c80fdd836dd8b1
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-06-24 17:01:34 +0630

    CI test

--- booking-system/obj/Debug/net8.0/booking-system.AssemblyInfo.cs
+++ booking-system/obj/Debug/net8.0/booking-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("booking-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0663501b37b3bf94b6477392b89160edb002357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b867979563c5c2bd9a503348b717c802339a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("booking-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("booking-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
